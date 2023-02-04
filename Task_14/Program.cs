57678210d586452ce1eb136b6dbaa605537ff0f1
GSY <gle722@yandex.ru>
2023-02-04 11:34:41 +0300

    Решили задание 22

---
+++ Task_14/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 0) num = -num;
+int check = 1;
+
+// while(check <= num)
+// {
+//     Console.Write($"{Math.Pow(check, 2)} ");
+//     if (check != a)
+//     {
+//         Console.Write(", ");
+//     }   
+//     check += 1;
+// }
+
+while(check <= num)
+{
+    Console.Write($"{check * check} ");
+    if (check != num)
+    {
+        Console.Write(", ");
+    }
+    check += 1;
+}
+
