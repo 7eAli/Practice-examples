295d7a51bfada904eb9f268c4c33fb3cfae48123
GSY <gle722@yandex.ru>
2023-02-05 10:50:38 +0300

    Выполнили задание 2 семинара 4

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Возведите число А в натуральную степень B используя цикл */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("В какую степень возвести число?: ");
+int b = int.Parse(Console.ReadLine()!);
+double result = a;
+if(b > 0)
+{
+for (int i = 1; i < b; i++)
+{
+    result = result * a;
+}
+}
+else
+{
+    b = -b;
+    for (int i = 1; i < b; i++)
+{
+    result = result * a;
+}
+result = 1/result;
+}
+Console.WriteLine(result);
