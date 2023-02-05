562d98caa1f6c7023a98ad78f8f44aa71348e342
GSY <gle722@yandex.ru>
2023-02-05 11:29:43 +0300

    Решили задание 3 семинара 4

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить количество цифр в числе
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int counter = 0;
+if (num == 0) counter = 1;
+else
+{
+    while (num != 0)
+    {
+        num /= 10;
+        counter++;
+    }
+}
+Console.WriteLine($"В числе {counter} цифр");
