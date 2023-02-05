7866d185674a567b079f95ead1de86a3bff95984
GSY <gle722@yandex.ru>
2023-02-05 10:30:12 +0300

    Сделали 1 задание 4 семинара

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Найти сумму чисел от 1 до A */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+if (num > 0)
+{
+for (int i = 0; i <= num; i++)
+{
+    sum = sum + i;
+}
+}
+else
+{
+   for (int i = 0; i >= num; i--)
+   {
+    sum = sum + i;
+   } 
+}
+Console.WriteLine(sum);
