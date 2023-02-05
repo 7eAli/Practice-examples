3cef9c56802b48733307075ab7b8b279a29e5cb5
GSY <gle722@yandex.ru>
2023-02-05 11:43:55 +0300

    Добавил альтернативное решение к 3 заданию 4 семинара

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -14,3 +14,18 @@ else
     }
 }
 Console.WriteLine($"В числе {counter} цифр");
+// Определить количество цифр в числе
+// Console.Clear();
+
+// Console.Write("Введите число: "); 
+// int a = Convert.ToInt32(Console.ReadLine()!);
+
+// int count = 1;
+
+// while (a >= 10 || a <= -10)
+// {
+//     a = a / 10;
+//     count ++;
+// }
+
+// Console.WriteLine(count);
