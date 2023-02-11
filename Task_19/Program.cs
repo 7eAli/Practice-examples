76824ea57baa93aad3b6bf2ab7b25ddf6e5cd955
GSY <gle722@yandex.ru>
2023-02-11 10:45:11 +0300

    Выполнили задание 32

---
+++ Task_19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ChangeSign(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        //inArray[i] = -inArray[i];
+        inArray[i] *= -1;
+    }        
+}
+
+Console.Clear();
+int[] array = GetArray(10, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+ChangeSign(array);
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = -array[i];
+// }
+
+Console.WriteLine(string.Join(", ", array));
