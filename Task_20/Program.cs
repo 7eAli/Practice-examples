0c1373e6de18aaffedc2c16ef835499e378f169d
GSY <gle722@yandex.ru>
2023-02-11 11:02:43 +0300

    Выполнили задание 33

---
+++ Task_20/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+bool FindNumber(int number, int[] inArray)
+{   
+    foreach(int element in inArray)
+    {
+        if (element == number) return true;
+    }
+    return false;
+}
+
+int[] array = GetArray(10, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+// bool check = FindNumber(number, array);
+if (FindNumber(number, array) == true) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
