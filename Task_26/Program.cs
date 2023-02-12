53dc4b81f77b7207420e5ab621ffcfc7195217dd
GSY <gle722@yandex.ru>
2023-02-12 11:19:15 +0300

    Решили задание 44

---
+++ Task_26/Program.cs
@@ -0,0 +1,41 @@
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+// int Fibonacci(int n)
+// {
+//     int elemOne = 0;
+//     int elemTwo = 1;
+//     int result = elemOne + elemTwo;
+//     for (int i = 3; i <= n; i++)
+//     {
+//         elemOne = elemTwo;
+//         elemTwo = result;
+//         result = elemOne + elemTwo;
+//     }
+//     return result;
+// }
+
+// Console.Clear();
+// for (int i = 2; i < 50; i++)
+// {
+//     Console.WriteLine(Fibonacci(i));
+// }
+
+double[] Fibonacci(int N)
+{
+    double[] array = new double[N];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+Console.Clear();
+
+Console.Write("Сколько чисел Фибоначи вывести? ");
+int N = int.Parse(Console.ReadLine()!);
+double[] array = Fibonacci(N);
+Console.WriteLine(string.Join(", ", array));
