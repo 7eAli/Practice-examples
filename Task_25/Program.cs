a76f94952d2e13230f4168876e56516353743fd1
GSY <gle722@yandex.ru>
2023-02-12 11:04:44 +0300

    Решили задание 42

---
+++ Task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное
+
+string ConverDecToBin(int num)
+{
+    string result = "";
+    while(num > 0)
+    {
+        result = (num % 2).ToString() + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()!);
+string binNum = ConverDecToBin(num);
+Console.WriteLine(binNum);
