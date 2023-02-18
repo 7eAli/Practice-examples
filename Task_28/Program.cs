11f686b4b08009a03eda36a29f3afd82cc5bdc30
GSY <gle722@yandex.ru>
2023-02-18 10:29:16 +0300

    Решили демонстрационную задачу

---
+++ Task_28/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задайте двумерный массив размером m x n,
+ заполненный случайными целыми числами */
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, column, -10, 10);
+PrintArray(array);
