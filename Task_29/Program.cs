30d105d05af8df36073fdf5fdd964ab0b0a9a560
GSY <gle722@yandex.ru>
2023-02-18 10:40:18 +0300

    Решили задание 49

---
+++ Task_29/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aij = i+j. 
+Выведите полученный массив на экран. */
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine()!);
+
+int[,] array = GetArray(row, column);
+PrintArray(array);
