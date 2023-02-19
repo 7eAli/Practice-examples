4b7bda90d2e30b466980f87efc2f960aac0a8b08
GSY <gle722@yandex.ru>
2023-02-19 11:02:11 +0300

    Решили задание 55

---
+++ Task_33/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя. */
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FlipRowsAndColumns(int[,] array)
+{
+    int[,] temp = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            temp[i, j] = array[j, i];
+        }
+    }
+    return temp;
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, column, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+if (array.GetLength(0) == array.GetLength(1))
+{
+    PrintArray(FlipRowsAndColumns(array));
+}
+else
+{
+    Console.WriteLine("Поменять местами строки и столбцы нельзя");
+}
