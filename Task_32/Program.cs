2e2a8949387a8afed9fff85e2d8bf7b292b6ef1c
GSY <gle722@yandex.ru>
2023-02-19 10:30:24 +0300

    Решили задание 53

---
+++ Task_32/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+которая поменяет местами первую и последнюю строку
+массива. */
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FlipRows(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+    }
+}
+
+int row = 5;
+int column = 5;
+int minValue = 1;
+int maxValue = 10;
+
+int[,] array = GetArray(row, column, minValue, maxValue);
+PrintArray(array);
+FlipRows(array);
+Console.WriteLine();
+PrintArray(array);
+
