57a65c5131ad91f6dc49b2539c09d9356bf1c610
GSY <gle722@yandex.ru>
2023-02-25 11:00:02 +0300

    решили задание 63

---
+++ Task_37/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+
+string NumbersList(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {        
+        return NumbersList(N - 1) + ", " + N.ToString();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+string list = NumbersList(N);
+Console.WriteLine(list);
