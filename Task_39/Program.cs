a43fc1b5598e100335d40ddcef8255654fa8d6ca
GSY <gle722@yandex.ru>
2023-02-25 11:19:49 +0300

    Решили задание 67

---
+++ Task_39/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+int DigitSum(int number)
+{
+    if (number == 0) return 0;
+    else
+    {        
+        return number % 10 + DigitSum(number / 10);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int digSum = DigitSum(number);
+Console.WriteLine($"Сумма чисел у {number} равна {digSum}");
